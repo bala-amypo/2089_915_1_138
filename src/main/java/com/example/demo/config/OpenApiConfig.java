7d5e6af0b7f9e51465364396a8b8ad4be7fa0b92
bala-amypo <bala@amypo.in>
2025-12-22 04:31:47 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,13 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -16,19 +12,10 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Hotel Room Key Digital Share API")
-                        .description("API for managing hotel guests, room bookings, digital keys, key sharing, and access logging")
-                        .version("1.0.0"))
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("Bearer Authentication",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
+                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9262.pro604cr.amypo.ai/") 
+                        // new Server().url("https://9005.vs.amypo.ai")
+                        new Server().url("https://9056.pro604cr.amypo.ai/")
                 ));
-    }
+        }
 }
